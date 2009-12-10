4d1cc3b22d23c4b4325f602279d5fbf16d83b440
Justin Hileman <justin@justinhileman.info>
2009-12-10 21:41:51 +1100

    PHP 5.3.5
    It includes support for:
    
     - MySQL
     - MSSQL
     - PostgreSQL
     - intl (icu4c)
     - Apache
     - PHP-FPM
    
    This is a combination of 33 commits originally by:
    
    Luke Gallagher <notfornoone@gmail.com>
    Justin Hileman <justin@justinhileman.info>
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ duplicates/php.rb
@@ -0,0 +1,194 @@
+require 'formula'
+
+def mysql_installed?
+  `which mysql_config`.length > 0
+end
+
+class Php <Formula
+  url 'http://www.php.net/get/php-5.3.5.tar.gz/from/this/mirror'
+  homepage 'http://php.net/'
+  md5 'fb727a3ac72bf0ce37e1a20468a7bb81'
+  version '5.3.5'
+
+  # So PHP extensions don't report missing symbols
+  skip_clean ['bin', 'sbin']
+
+  depends_on 'gettext'
+  depends_on 'readline' unless ARGV.include? '--without-readline'
+  depends_on 'libxml2'
+  depends_on 'jpeg'
+  depends_on 'libpng'
+  depends_on 'mcrypt'
+
+  if ARGV.include? '--with-mysql'
+    depends_on 'mysql' => :recommended unless mysql_installed?
+  end
+  if ARGV.include? '--with-fpm'
+    depends_on 'libevent'
+  end
+  if ARGV.include? '--with-pgsql'
+    depends_on 'postgresql'
+  end
+  if ARGV.include? '--with-mssql'
+    depends_on 'freetds'
+  end
+  if ARGV.include? '--with-intl'
+    depends_on 'icu4c'
+  end
+
+  def options
+   [
+     ['--with-mysql', 'Include MySQL support'],
+     ['--with-pgsql', 'Include PostgreSQL support'],
+     ['--with-mssql', 'Include MSSQL-DB support'],
+     ['--with-fpm', 'Enable building of the fpm SAPI executable (implies --without-apache)'],
+     ['--without-apache', 'Build without shared Apache 2.0 Handler module'],
+     ['--with-intl', 'Include internationalization support'],
+     ['--without-readline', 'Build without readline support']
+   ]
+  end
+
+  def patches; DATA; end
+
+  def configure_args
+    args = [
+      "--prefix=#{prefix}",
+      "--disable-debug",
+      "--with-config-file-path=#{etc}",
+      "--with-iconv-dir=/usr",
+      "--enable-exif",
+      "--enable-soap",
+      "--enable-sqlite-utf8",
+      "--enable-wddx",
+      "--enable-ftp",
+      "--enable-sockets",
+      "--enable-zip",
+      "--enable-pcntl",
+      "--enable-shmop",
+      "--enable-sysvsem",
+      "--enable-sysvshm",
+      "--enable-sysvmsg",
+      "--enable-mbstring",
+      "--enable-bcmath",
+      "--enable-calendar",
+      "--with-openssl=/usr",
+      "--with-zlib=/usr",
+      "--with-bz2=/usr",
+      "--with-ldap",
+      "--with-ldap-sasl=/usr",
+      "--with-xmlrpc",
+      "--with-iodbc",
+      "--with-kerberos=/usr",
+      "--with-libxml-dir=#{Formula.factory('libxml2').prefix}",
+      "--with-xsl=/usr",
+      "--with-curl=/usr",
+      "--with-gd",
+      "--enable-gd-native-ttf",
+      "--with-mcrypt=#{Formula.factory('mcrypt').prefix}",
+      "--with-jpeg-dir=#{Formula.factory('jpeg').prefix}",
+      "--with-png-dir=#{Formula.factory('libpng').prefix}",
+      "--with-gettext=#{Formula.factory('gettext').prefix}",
+      "--with-tidy",
+      "--mandir=#{man}"
+    ]
+
+    # Enable PHP FPM
+    if ARGV.include? '--with-fpm'
+      args.push "--enable-fpm"
+    end
+
+    # Build Apache module by default
+    unless ARGV.include? '--with-fpm' or ARGV.include? '--without-apache'
+      args.push "--with-apxs2=/usr/sbin/apxs"
+      args.push "--libexecdir=#{prefix}/libexec"
+    end
+
+    if ARGV.include? '--with-mysql'
+      args.push "--with-mysql-sock=/tmp/mysql.sock"
+      args.push "--with-mysqli=mysqlnd"
+      args.push "--with-mysql=mysqlnd"
+      args.push "--with-pdo-mysql=mysqlnd"
+    end
+
+    if ARGV.include? '--with-pgsql'
+      args.push "--with-pgsql=#{Formula.factory('postgresql').prefix}"
+      args.push "--with-pdo-pgsql=#{Formula.factory('postgresql').prefix}"
+    end
+
+    if ARGV.include? '--with-mssql'
+      args.push "--with-mssql=#{Formula.factory('freetds').prefix}"
+    end
+
+    if ARGV.include? '--with-intl'
+      args.push "--enable-intl"
+      args.push "--with-icu-dir=#{Formula.factory('icu4c').prefix}"
+    end
+
+    args.push "--with-readline=#{Formula.factory('readline').prefix}" unless ARGV.include? '--without-readline'
+
+    return args
+  end
+  
+  def install
+    ENV.O3 # Speed things up
+    system "./configure", *configure_args
+
+    unless ARGV.include? '--without-apache'
+      # Use Homebrew prefix for the Apache libexec folder
+      inreplace "Makefile",
+        "INSTALL_IT = $(mkinstalldirs) '$(INSTALL_ROOT)/usr/libexec/apache2' && $(mkinstalldirs) '$(INSTALL_ROOT)/private/etc/apache2' && /usr/sbin/apxs -S LIBEXECDIR='$(INSTALL_ROOT)/usr/libexec/apache2' -S SYSCONFDIR='$(INSTALL_ROOT)/private/etc/apache2' -i -a -n php5 libs/libphp5.so",
+        "INSTALL_IT = $(mkinstalldirs) '#{prefix}/libexec/apache2' && $(mkinstalldirs) '$(INSTALL_ROOT)/private/etc/apache2' && /usr/sbin/apxs -S LIBEXECDIR='#{prefix}/libexec/apache2' -S SYSCONFDIR='$(INSTALL_ROOT)/private/etc/apache2' -i -a -n php5 libs/libphp5.so"
+    end
+
+    if ARGV.include? '--with-intl'
+      inreplace 'Makefile' do |s|
+        s.change_make_var! "EXTRA_LIBS", "\\1 -lstdc++"
+      end
+    end
+
+    system "make"
+    system "make install"
+
+    system "cp ./php.ini-production #{etc}/php.ini" unless File.exists? "#{etc}/php.ini"
+  end
+
+ def caveats; <<-EOS
+For 10.5 and Apache:
+    Apache needs to run in 32-bit mode. You can either force Apache to start 
+    in 32-bit mode or you can thin the Apache executable.
+
+To enable PHP in Apache add the following to httpd.conf and restart Apache:
+    LoadModule php5_module    #{prefix}/libexec/apache2/libphp5.so
+
+The php.ini file can be found in:
+    #{etc}/php.ini
+
+'Fix' the default PEAR permissions and config:
+    chmod -R ug+w #{prefix}/lib/php
+    pear config-set php_ini #{etc}/php.ini
+   EOS
+ end
+end
+
+
+__END__
+diff -Naur php-5.3.2/ext/tidy/tidy.c php/ext/tidy/tidy.c 
+--- php-5.3.2/ext/tidy/tidy.c	2010-02-12 04:36:40.000000000 +1100
++++ php/ext/tidy/tidy.c	2010-05-23 19:49:47.000000000 +1000
+@@ -22,6 +22,8 @@
+ #include "config.h"
+ #endif
+ 
++#include "tidy.h"
++
+ #include "php.h"
+ #include "php_tidy.h"
+ 
+@@ -31,7 +33,6 @@
+ #include "ext/standard/info.h"
+ #include "safe_mode.h"
+ 
+-#include "tidy.h"
+ #include "buffio.h"
+ 
+ /* compatibility with older versions of libtidy */
