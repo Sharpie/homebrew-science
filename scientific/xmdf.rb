76069494decdad8f88c9e368a1aa979a384cd16c
Charlie Sharpsteen <source@sharpsteen.net>
2011-03-08 14:04:22 -0800

    Added formula for XMDF
    This formula provides a brew for the XMDF (eXtensible Model Data Format)
    library. XMDF is a data format built on top of the HDF5 library that is used
    for storing the input and output of some scientific models.

---
+++ scientific/xmdf.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Xmdf <Formula
+  homepage 'http://www.xmdf.org'
+  url 'http://dl.dropbox.com/u/72178/dist/xmdf-unix-1.7.tar.gz'
+  md5 'a7d1769044a782b0d2f4c286d831fd9d'
+
+  depends_on 'hdf5'
+
+  depends_on 'cmake' => :build
+  depends_on 'gfortran' => :build
+
+  def install
+    Dir.chdir('build') do
+      system "cmake .. #{std_cmake_parameters}"
+      system "make install"
+    end
+  end
+
+  def caveats
+    caveats = <<-EOS
+This is an unoffical re-packaging of the XMDF source code with a CMake build
+system. For the origional souce code and documentation, see:
+
+    http://www.xmdf.org
+    EOS
+  end
+end
