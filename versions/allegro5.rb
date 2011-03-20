3f5e10dd758fa2e33b08e93c0568446350f5e326
Jorge Luis Mendez <jorge@jorgemendez.me>
2011-03-19 22:28:46 -0500

    Versioned formula for allegro 5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ versions/allegro5.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Allegro5 < Formula
+  url 'http://downloads.sourceforge.net/project/alleg/allegro/5.0.1/allegro-5.0.1.tar.gz'
+  homepage 'http://www.allegro.cc'
+  md5 '7a7dafa3362b97dd653f736e21f5ef21'
+
+  depends_on 'cmake' => :build
+  depends_on 'libvorbis' => :optional
+
+  def install
+    system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
