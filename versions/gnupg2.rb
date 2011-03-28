9b41f6708c39a96177fbd8249bd0f88ba3a6b122
Jack Nagel <jacknagel@gmail.com>
2011-03-28 16:43:11 -0500

    New Formula: gnupg2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ versions/gnupg2.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Gnupg2 < Formula
+  url 'ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.0.17.tar.bz2'
+  homepage 'http://www.gnupg.org/'
+  sha1 '41ef5460417ca0a1131fc730849fe3afd49ad2de'
+
+  depends_on 'libgpg-error'
+  depends_on 'libgcrypt'
+  depends_on 'libksba'
+  depends_on 'libassuan'
+  depends_on 'pinentry'
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
+    system "make"
+    system "make install"
+  end
+end
