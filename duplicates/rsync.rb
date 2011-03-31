8f7cd108336441671f70cebe26e23e70b98ad1cc
Aiden Scandella <aiden.scandella@socrata.com>
2011-03-30 17:54:09 -0700

    rsync - source url was incorrect
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- duplicates/rsync.rb
+++ duplicates/rsync.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Rsync <Formula
-  url 'http://rsync.samba.org/ftp/rsync/rsync-3.0.7.tar.gz'
+  url 'http://rsync.samba.org/ftp/rsync/src/rsync-3.0.7.tar.gz'
   homepage 'http://rsync.samba.org/'
   md5 'b53525900817cf1ba7ad3a516ab5bfe9'
 
