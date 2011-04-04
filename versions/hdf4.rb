ac718fb4bcf08a6f5622627280c678b19ee485bf
Charlie Sharpsteen <source@sharpsteen.net>
2011-04-03 18:26:56 -0700

    HDF4 formula updates
    Two tweaks to the HDF4 Formula:
    
    * Bring Fortran support in the HDF4 formula up to date with Homebrew
      standards.
    
    * Enable the installation of the `h4cc` and `h4fc` compiler wrappers generated
      by HDF4. These wrapper scripts are sometimes required by other packages that
      depend on HDF4.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- versions/hdf4.rb
+++ versions/hdf4.rb
@@ -1,5 +1,9 @@
 require 'formula'
 
+def fortran?
+  ARGV.include? '--enable-fortran'
+end
+
 class Jpeg6 <Formula
   url 'http://www.hdfgroup.org/ftp/lib-external/jpeg/src/jpegsrc.v6b.tar.gz'
   md5 '83992a9466af7536da30efe6b51d4064'
@@ -14,9 +18,34 @@ class Hdf4 <Formula
 
   depends_on "szip"
 
-  def install
-    jpeg_dir = Pathname.new( Dir.pwd ) + 'jpeg6'
+  def options
+    [
+      ['--enable-fortran', 'Build Fortran libraries']
+    ]
+  end
 
+  def install_wrapper compiler_wrapper
+    # The HDF4 compiler wrappers `h4cc` and `h4fc` are expected to be present by
+    # some libraries that depend on HDF4---they serve the same purpose as
+    # `*-config` scripts for other packages.
+    inreplace compiler_wrapper do |s|
+      # Reset paths
+      s.change_make_var! 'prefix', prefix
+      s.change_make_var! 'exec_prefix', prefix
+      s.change_make_var! 'libdir', lib
+      s.change_make_var! 'includedir', include
+      # Remove references to Jpeg6---it gets statically compiled into the HDF4
+      # libraries
+      s.gsub! /-I[^\s]*\/jpeg6\/include/, ''
+      s.gsub! /-L[^\s]*\/jpeg6\/lib/, ''
+    end
+    bin.install compiler_wrapper
+  end
+
+  def install
+    ENV.fortran if fortran?
+
+    jpeg_dir = Pathname.new(Dir.pwd) + 'jpeg6'
     Jpeg6.new.brew do
       system "./configure", "--disable-shared"
       system "make"
@@ -28,28 +57,33 @@ class Hdf4 <Formula
       jpeg_include.install Dir['*.h']
     end
 
-    hdf_stage = Pathname.new( Dir.pwd ) + 'build'
+    hdf_stage = Pathname.new(Dir.pwd) + 'build'
+    args = [
+      "--prefix=#{hdf_stage}",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--enable-production",
+      "--disable-netcdf",
+      "--disable-shared",
+      "--with-jpeg=#{jpeg_dir}",
+      "--with-szlib=#{HOMEBREW_PREFIX}"
+    ]
+    args << '--disable-fortran' unless fortran?
 
-    system "./configure", "--prefix=#{hdf_stage}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--enable-production",
-                          "--disable-netcdf",
-                          "--disable-shared",
-                          "--with-jpeg=#{jpeg_dir}",
-                          "--with-szlib=#{HOMEBREW_PREFIX}"
-
+    system "./configure", *args
     system "make install"
 
     Dir.chdir hdf_stage do
-      # We only install libs and includes.  Due to the age of this code, all
-      # binaries want to use JPEG as a dynamic dependency--- but they get a
-      # system library that is much newer than expected.  Also, for some strange
-      # reason HDF4 builds the `ncdump` and `ncgen` utilities even though
-      # `--disable-netcdf` was passed to configure.  Therefore, installing the
-      # bin folder is dangerous.
+      # We only install libs, includes and compiler wrapper scripts. Due to the
+      # age of this code, most binaries want to use JPEG as a dynamic
+      # dependency---but they get a system library that is much newer than
+      # expected.  Also, for some strange reason HDF4 builds the `ncdump` and
+      # `ncgen` utilities even though `--disable-netcdf` was passed to
+      # configure.  Therefore, installing the bin folder is dangerous.
       lib.install Dir['lib/*']
       include.install Dir['include/*']
+      install_wrapper Dir['bin/h4cc']
+      install_wrapper Dir['bin/h4fc'] if fortran?
     end
   end
 
