ae8245cf8a9ae4921dedff3fa308de11076ed267
Gary V. Vaughan <gary@gnu.org>
2011-04-05 13:52:25 +0700

    autoconf 2.68
    The system autoconf shipped by Apple is too old and buggy to successfully
    bootstrap many current free software projects.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ duplicates/autoconf.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Autoconf < Formula
+  url 'ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.68.tar.gz'
+  homepage 'http://www.gnu.org/software/autoconf'
+  md5 'c3b5247592ce694f7097873aa07d66fe'
+
+  keg_only <<-EOS.undent
+    The OSX shipped autoconf is more than 5 years old, and missing many
+    critical bug-fixes compared with the latest upstream.
+    In order to prevent conflicts with the system autoconf, this formula
+    is keg-only.
+  EOS
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
