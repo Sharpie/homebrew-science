f5a03c0d42f9a8c24aebefaa75fad4091bd71227
Jack Nagel <jacknagel@gmail.com>
2011-05-03 18:25:48 -0500

    rsync 3.0.8
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- duplicates/rsync.rb
+++ duplicates/rsync.rb
@@ -1,24 +1,24 @@
 require 'formula'
 
 class Rsync < Formula
-  url 'http://rsync.samba.org/ftp/rsync/src/rsync-3.0.7.tar.gz'
+  url 'http://rsync.samba.org/ftp/rsync/src/rsync-3.0.8.tar.gz'
   homepage 'http://rsync.samba.org/'
-  md5 'b53525900817cf1ba7ad3a516ab5bfe9'
+  md5 '0ee8346ce16bdfe4c88a236e94c752b4'
 
   depends_on 'libiconv'
 
   def patches
-    base = "http://trac.macports.org/export/65950/trunk/dports/net/rsync/files"
-    {
-      :p1 => ["#{base}/patch-fileflags.diff", "#{base}/patch-crtimes.diff"]
-    }
+    base = "http://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.0.8;f="
+    ["#{base}fileflags.diff", "#{base}crtimes.diff", "#{base}hfs-compression.diff"]
   end
 
   def install
+    system "./prepare-source"
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--with-rsyncd-conf=#{prefix}/etc/rsyncd.conf",
                           "--enable-ipv6"
+    system "make"
     system "make install"
   end
 end
