28c9ff43408186125b0bfd1223ae829087d22d64
Mathieu Rhéaume <ddrmanxbxfr@gmail.com>
2011-05-03 10:22:12 -0400

    fetchmail 6.3.19
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- duplicates/fetchmail.rb
+++ duplicates/fetchmail.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fetchmail <Formula
   homepage 'http://www.fetchmail.info/'
-  url 'http://download.berlios.de/fetchmail/fetchmail-6.3.13.tar.bz2'
-  md5 'db792fb311bc358e95ed0437389269ac'
+  url 'http://prdownload.berlios.de/fetchmail/fetchmail-6.3.19.tar.bz2'
+  md5 '64519711c8533f5a34d20c9ff620d880'
 
   def install
     system "./configure", "--prefix=#{prefix}"
