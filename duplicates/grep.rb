6bd72dc3f20c28a5015096d503a2432dc5a403f7
Jack Nagel <jacknagel@gmail.com>
2011-05-22 18:31:35 -0500

    grep 2.8
    It is no longer necessary to patch the Makefile to get -liconv as this
    was incorporated upstream.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- duplicates/grep.rb
+++ duplicates/grep.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
-class Grep <Formula
+class Grep < Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://ftp.gnu.org/gnu/grep/grep-2.7.tar.gz'
-  md5 'e848f07e3e79aa7899345d17c7e4115e'
+  url 'http://ftp.gnu.org/gnu/grep/grep-2.8.tar.gz'
+  md5 'cb2dfc502c5afc7a4a6e5f6cefd6850e'
 
   depends_on 'pcre'
 
@@ -13,12 +13,7 @@ class Grep <Formula
                           "--prefix=#{prefix}",
                           "--infodir=#{info}",
                           "--mandir=#{man}"
-
-    # Configure gives me no -liconv, so fix that
-    inreplace "src/Makefile" do |s|
-      s.change_make_var! "LIBS", "-liconv -lpcre"
-    end
-
+    system "make"
     system "make install"
   end
 end
