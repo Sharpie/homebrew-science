bf15ea2ade25888a7f915f234d61ff7c2a8d4827
Ville Lautanala <lautis@gmail.com>
2011-06-29 23:17:36 +0300

    rtmpdump-yle 1.4.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- other/rtmpdump-yle.rb
+++ other/rtmpdump-yle.rb
@@ -1,12 +1,16 @@
 require 'formula'
 
 class RtmpdumpYle < Formula
-  depends_on 'json-c'
-  url 'http://users.tkk.fi/~aajanki/rtmpdump-yle/rtmpdump-yle-1.4.1.tar.gz'
+  url 'http://users.tkk.fi/~aajanki/rtmpdump-yle/rtmpdump-yle-1.4.2.tar.gz'
   homepage 'http://users.tkk.fi/~aajanki/rtmpdump-yle/index.html'
-  md5 '847b33a19161390d2dc568255af244d1'
+  md5 '5bdbf955d3b4ddb06eaa13b326d9d80d'
+
+  depends_on 'json-c'
 
   def install
+    # Help the formula find the json-c library
+    ENV["INC"] = "-I#{HOMEBREW_PREFIX}/include/json"
+
     system "make", "SYS=darwin"
     system "make", "install", "SYS=darwin", "prefix=#{prefix}", "mandir=#{man}"
   end
