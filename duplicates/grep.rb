fed4f4a3a1aa30416f7cf286a43eba19d015db36
Jack Nagel <jacknagel@gmail.com>
2011-07-04 13:52:00 -0500

    grep 2.9
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- duplicates/grep.rb
+++ duplicates/grep.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Grep < Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://ftp.gnu.org/gnu/grep/grep-2.8.tar.gz'
-  md5 'cb2dfc502c5afc7a4a6e5f6cefd6850e'
+  url 'http://ftp.gnu.org/gnu/grep/grep-2.9.tar.gz'
+  md5 '03e3451a38b0d615cb113cbeaf252dc0'
 
   depends_on 'pcre'
 
