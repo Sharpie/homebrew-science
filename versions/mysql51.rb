8caa6bc09dbe1b50be0b72840afb131eef1edb95
visnu pitiyanuvath <visnupx@gmail.com>
2011-07-06 10:12:25 -0700

    mysql 5.1.58
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- versions/mysql51.rb
+++ versions/mysql51.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mysql51 < Formula
   homepage 'http://dev.mysql.com/doc/refman/5.1/en/'
-  url 'http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.56.tar.gz'
-  md5 '15161d67f4830aad3a8a89e083749d49'
+  url 'http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.58.tar.gz'
+  md5 'ae5aef506088e521e4b1cc4f668e96d2'
 
   depends_on 'readline'
 
