845c54b38a95f6acf0143023fc936fd94f7c4e52
Jack Nagel <jacknagel@gmail.com>
2011-07-12 21:13:42 -0500

    fetchmail 6.3.21
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- duplicates/fetchmail.rb
+++ duplicates/fetchmail.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Fetchmail < Formula
   homepage 'http://www.fetchmail.info/'
-  url 'http://prdownload.berlios.de/fetchmail/fetchmail-6.3.19.tar.bz2'
-  md5 '64519711c8533f5a34d20c9ff620d880'
+  url 'http://prdownload.berlios.de/fetchmail/fetchmail-6.3.21.tar.bz2'
+  md5 '548f152df27d32b632afa4f1a8a6cd1a'
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
 end
