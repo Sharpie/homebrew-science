b9523911815e4576ff05e5a54a884c4724d57de6
Patrick Marschik <patrick.marschik@gmail.com>
2011-07-18 13:48:27 +0200

    tomcat 6.0.32
    Closes #62.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ versions/tomcat6.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class Tomcat6 < Formula
+  url 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.32/bin/apache-tomcat-6.0.32.tar.gz'
+  homepage 'http://tomcat.apache.org/'
+  md5 '928a960268adf610a7d6fe5c4fcd0b20'
+
+  skip_clean :all
+
+  keg_only "Some scripts that are installed conflict with other software."
+
+  def install
+    rm_rf Dir['bin/*.{cmd,bat]}']
+    libexec.install Dir['*']
+    (libexec+'logs').mkpath
+    bin.mkpath
+    Dir["#{libexec}/bin/*.sh"].each { |f| ln_s f, bin }
+  end
+
+  def caveats; <<-EOS.undent
+    Some of the support scripts used by Tomcat have very generic names.
+    These are likely to conflict with support scripts used by other Java-based
+    server software.
+
+    You can link Tomcat into PATH with:
+
+      brew link tomcat6
+
+    or add #{bin} to your PATH instead.
+    EOS
+  end
+end
