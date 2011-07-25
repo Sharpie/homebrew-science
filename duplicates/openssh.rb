f514099c4032773cbe74fa1dda72692650435f68
Corry <tabletcorry@gmail.com>
2011-07-25 13:29:43 -0700

    openssh 5.8p2
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- duplicates/openssh.rb
+++ duplicates/openssh.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Openssh < Formula
-  url 'ftp://ftp.lambdaserver.com/pub/OpenBSD/OpenSSH/portable/openssh-5.4p1.tar.gz'
+  url 'ftp://ftp.lambdaserver.com/pub/OpenBSD/OpenSSH/portable/openssh-5.8p2.tar.gz'
   homepage 'http://www.openssh.com/'
-  md5 'da10af8a789fa2e83e3635f3a1b76f5e'
-  version '5.4p1'
+  sha1 '64798328d310e4f06c9f01228107520adbc8b3e5'
+  version '5.8p2'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--with-libedit", "--prefix=#{prefix}"
