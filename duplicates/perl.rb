274f18a83246d1faf6b39ff2d45e57ff51690e17
Peter Marreck <lumbergh@gmail.com>
2011-07-27 12:45:37 -0300

    perl 5.14.1
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- duplicates/perl.rb
+++ duplicates/perl.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Perl < Formula
-  url 'http://www.cpan.org/src/5.0/perl-5.12.3.tar.gz'
+  url 'http://www.cpan.org/src/5.0/perl-5.14.1.tar.gz'
   homepage 'http://www.perl.org/'
-  md5 '29975a69dce54e47fcd6331c085c6c99'
+  md5 '0b74cffa3a10aee08442f950aecbaeec'
 
   def install
     system("rm -f config.sh Policy.sh");
