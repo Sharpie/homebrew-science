34d6589890d26fb7d69dcc4319eae0d439d24963
Adam Vandenberg <flangy@gmail.com>
2011-07-31 12:44:04 -0700

    vim 7.3.266

--- duplicates/vim.rb
+++ duplicates/vim.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Vim < Formula
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '951641b8784de5175cadf9fdbc361db2b6b45060'
-  version '7.3.102'
+  url 'https://vim.googlecode.com/hg/', :revision => '25be7c9dda54'
+  version '7.3.266'
   homepage 'http://www.vim.org/'
 
   head 'https://vim.googlecode.com/hg/'
