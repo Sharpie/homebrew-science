e6aa6eec214859c4e51b0f953ba2e7e4cb1fee66
Jack Nagel <jacknagel@gmail.com>
2011-08-15 20:12:39 -0500

    gnupg2: add dirmngr as an optional dependency
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- versions/gnupg2.rb
+++ versions/gnupg2.rb
@@ -11,8 +11,11 @@ class Gnupg2 < Formula
   depends_on 'libassuan'
   depends_on 'pinentry'
   depends_on 'pth'
+  depends_on 'dirmngr' => :optional
 
   def install
+    inreplace 'common/homedir.c', '/var/run', '#{var}/run'
+
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking"
     system "make"
