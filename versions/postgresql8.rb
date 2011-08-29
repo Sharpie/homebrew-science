aa11f3a83c8ab2f3f35ad247400882b5c9231ae0
Alexander Ivanov <tei.krsk@gmail.com>
2011-08-29 14:52:12 +0800

    postgresql 8.4.8
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- versions/postgresql8.rb
+++ versions/postgresql8.rb
@@ -3,8 +3,8 @@ require 'hardware'
 
 class Postgresql8 < Formula
   homepage 'http://www.postgresql.org/'
-  url 'http://ftp2.uk.postgresql.org/sites/ftp.postgresql.org/source/v8.4.7/postgresql-8.4.7.tar.bz2'
-  md5 '689397187bb1dfe9b5cbde99538311c3'
+  url 'http://ftp2.uk.postgresql.org/sites/ftp.postgresql.org/source/v8.4.8/postgresql-8.4.8.tar.bz2'
+  md5 '4603e8ea30cee97189b62b39022f2043'
 
   depends_on 'readline'
   depends_on 'libxml2' if MACOS_VERSION < 10.6 # Leopard libxml is too old
