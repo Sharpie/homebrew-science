962dac0cee6435ea23e441008dd330dd70e78fb7
Malte S. Stretz <mss@apache.org>
2011-10-05 11:03:42 +0200

    rsync 3.0.9
    Closes #110.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- duplicates/rsync.rb
+++ duplicates/rsync.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Rsync < Formula
-  url 'http://rsync.samba.org/ftp/rsync/src/rsync-3.0.8.tar.gz'
+  url 'http://rsync.samba.org/ftp/rsync/src/rsync-3.0.9.tar.gz'
   homepage 'http://rsync.samba.org/'
-  md5 '0ee8346ce16bdfe4c88a236e94c752b4'
+  md5 '5ee72266fe2c1822333c407e1761b92b'
 
   depends_on 'libiconv'
 
   def patches
-    base = "http://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.0.8;f="
+    base = "http://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.0.9;f="
     ["#{base}fileflags.diff", "#{base}crtimes.diff", "#{base}hfs-compression.diff"]
   end
 
