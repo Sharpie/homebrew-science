633d737231ea7e45807f7e97d1dfc2aa370db250
Jack Nagel <jacknagel@gmail.com>
2011-10-09 19:09:11 -0500

    kindlegen: fix audit warnings
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- non-free/kindlegen.rb
+++ non-free/kindlegen.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
-class Kindlegen <Formula
+class Kindlegen < Formula
   url 'http://s3.amazonaws.com/kindlegen/KindleGen_Mac_i386_v1.2.zip'
   homepage 'http://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000234621'
   md5 '2a156d26ef337d3feb1e47fcd1e1e698'
-  version '1.2'
 
   def install
-    mkdir bin
-    
+    bin.mkpath
+
     prefix.install Dir['*']
-    (bin/'kindlegen').make_link(prefix/'kindlegen')
+    chmod 0755, prefix+'kindlegen'
+    ln_s prefix+'kindlegen', bin+'kindlegen'
   end
 
   def caveats; <<-EOS
