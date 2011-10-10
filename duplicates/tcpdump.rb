8ef7b389af28c18f5af33a4f08dd6c41298c7c74
Jack Nagel <jacknagel@gmail.com>
2011-10-09 19:40:42 -0500

    tcpdump: fix audit warning
    libpcap is provided by OS X, but for some reason we have to clear the
    CFLAGS for configure to find it correctly.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- duplicates/tcpdump.rb
+++ duplicates/tcpdump.rb
@@ -4,10 +4,10 @@ class Tcpdump < Formula
   head 'https://github.com/mcr/tcpdump.git'
   homepage 'http://www.tcpdump.org/'
 
-  depends_on 'libpcap'
-
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking","--disable-smb"
+    ENV.delete 'CFLAGS'
+    system "./configure", "--prefix=#{prefix}", "--disable-debug",
+                          "--disable-dependency-tracking", "--disable-smb"
     system "make install"
   end
 end
