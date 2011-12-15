6da6759b951512e70583be3892a7242b7ccc48d1
Jack Nagel <jacknagel@gmail.com>
2011-12-15 00:15:50 -0600

    grep 2.10
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- duplicates/grep.rb
+++ duplicates/grep.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Grep < Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://ftpmirror.gnu.org/grep/grep-2.9.tar.gz'
-  md5 '03e3451a38b0d615cb113cbeaf252dc0'
+  url 'http://ftpmirror.gnu.org/grep/grep-2.10.tar.xz'
+  md5 '709915434d09f5db655e5ebd7fd6bb7f'
 
   depends_on 'pcre'
 
