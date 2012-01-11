4091a14f95c60426a57f04f03c5d2197c7442d29
Justin Hileman <justin@justinhileman.info>
2012-01-10 20:07:40 -0800

    PHP 5.3.9
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- duplicates/php.rb
+++ duplicates/php.rb
@@ -9,10 +9,10 @@ def postgres_installed?
 end
 
 class Php < Formula
-  url 'http://www.php.net/get/php-5.3.8.tar.bz2/from/this/mirror'
+  url 'http://www.php.net/get/php-5.3.9.tar.bz2/from/this/mirror'
   homepage 'http://php.net/'
-  md5 '704cd414a0565d905e1074ffdc1fadfb'
-  version '5.3.8'
+  md5 'dd3288ed5c08cd61ac5bf619cb357521'
+  version '5.3.9'
 
   # So PHP extensions don't report missing symbols
   skip_clean ['bin', 'sbin']
